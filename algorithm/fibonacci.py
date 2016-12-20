01df137fde42400c92634c03dde29a8e4f651d80
dev4love <cantywei@gmail.com>
2016-12-20 17:55:31 +0800

    Add fibonacci and yh_triangle code.

---
+++ algorithm/fibonacci.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+def fibonacci(length):
+    """
+    指的是这样一个数列：1、1、2、3、5、8、13、21、34。
+    在数学上，斐波纳契数列被以递归的方法定义：F(1)=1，F(2)=1, F(n)=F(n-1)+F(n-2)（n>2，n∈N*）
+    """
+    n, a, b = 0, 0, 1
+    while n < length:
+        yield b
+        a, b = b, a + b
+        n += 1
+    print('done')
+
+if __name__ == u'__main__':
+    for i in fibonacci(5):
+        print(i)
---
+++ algorithm/yh_triangle.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+def yh_triangle(columns):
+    """
+    前提：每行端点与结尾的数为1.
+    每个数等于它上方两数之和。
+    每行数字左右对称，由1开始逐渐变大。
+    第n行的数字有n项。
+    第n行数字和为2n-1。
+    ...
+              1
+            1   1
+          1   2   1
+        1   3   3   1
+      1   4   6   4   1
+    1   5   10  10  5   1
+    """
+    b = [1]
+    n = 0
+    while n < columns:
+        yield b
+        b = [1] + [b[i] + b[i + 1] for i in range(len(b) - 1)] + [1]
+        n += 1
+
+if __name__ == u'__main__':
+    for line in yh_triangle(6):
+        print line
