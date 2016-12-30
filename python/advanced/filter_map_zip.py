69985589a87c84a2d61c763d684f4d76436021f4
dev4love <cantywei@gmail.com>
2016-12-30 14:47:18 +0800

    Update the usage of zip(unzip a list/tuple).

--- python/advanced/filter_map_zip.py
+++ python/advanced/filter_map_zip.py
@@ -3,9 +3,10 @@
 演示 filter、 map 和 zip 的用法:
 fileter 将不满足条件的元素过滤掉
 map 将元素处理后返回
-zip 以两（多）个序列中的最短长度为标准，将这些序列中角标位置相同的元素组合成一个元组（tuple），并返回所有元组的列表
+zip 以两（多）个序列中的最短长度为标准，将这些序列中角标位置相同的元素组合成一个元组（tuple），并返回所有元组的列表。如果 zip 的参数以 *list/tuple 方式传入，则会先将该 list/tuple 中的元素取出来，再对这些元素进行组合，返回组合后的 list。
 """
 __author__ = 'lovexiaov'
+
 
 def func(num):
     return True if num > 5 else False
@@ -19,9 +20,11 @@ nums = [1, 3, 5, 7, 9]
 res = list(filter(func, nums))
 print(res)  # [7, 9]
 
-res = list(map(lambda x: x*x, nums))
-print(res) # [1, 9, 25, 49, 81]
+res = list(map(lambda x: x * x, nums))
+print(res)  # [1, 9, 25, 49, 81]
 
 chars = ['a', 'b', 'c', 'd']
 res = list(zip(nums, chars))
-print(res) # [(1, 'a'), (3, 'b'), (5, 'c'), (7, 'd')]
+print(res)  # [(1, 'a'), (3, 'b'), (5, 'c'), (7, 'd')]
+u = zip(*res)
+print(u)  # [(1, 3, 5, 7), ('a', 'b', 'c', 'd')]
