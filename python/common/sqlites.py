acbd9a1d62ebdb8e33f71a6ae479a23f08a4cde4
dev4love <cantywei@gmail.com>
2017-01-19 17:41:05 +0800

    Add html&css sample.

--- python/common/sqlites.py
+++ python/common/sqlites.py
@@ -6,26 +6,31 @@ __author__ = u'lovexiaov'
 
 import sqlite3
 
-db = sqlite3.connect('test.db') # open sqlite db
+db = sqlite3.connect('test.db')  # open sqlite db
+
+
 def create_table():
     sql = ''' CREATE TABLE person
         (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         name TEXT NOT NULL,
         age INTEGER NOT NULL,
         addr CHAR(50));'''
-    db.execute(sql) # create table
+    db.execute(sql)  # create table
+
 
 def insert():
-    sql = '''INSERT INTO person (name, age, addr) 
+    sql = '''INSERT INTO person (name, age, addr)
         VALUES ('lovexiaov', 23, 'chaoyang, beijing');'''
     db.execute(sql)
     db.commit()
+
 
 def query():
     sql = '''SELECT _id, name FROM person;'''
     cursor = db.execute(sql)
     for row in cursor:
-        print('id: %s, name: %s' %row)
+        print('id: %s, name: %s' % row)
+
 
 def update():
     sql = '''UPDATE person SET name = "zhangsan" WHERE _id = 1;'''
@@ -33,11 +38,13 @@ def update():
     db.commit()
     query()
 
+
 def delete():
     sql = '''DELETE FROM person where name = "lovexiaov";'''
     db.execute(sql)
     db.commit()
-    
+
+
 if __name__ == u'__main__':
     # create_table()
     # insert()
@@ -45,7 +52,7 @@ if __name__ == u'__main__':
     # update()
     insert()
     query()
-    print('==='*3)
+    print('===' * 3)
     delete()
     query()
     db.close()
