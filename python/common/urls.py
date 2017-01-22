c0fbe1e7f9e4b9ddb13f16aed87f52088a68082b
dev4love <cantywei@gmail.com>
2017-01-22 11:23:56 +0800

    Add urls.py module。

---
+++ python/common/urls.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+"""
+This module demonstrate how to encode and decode urls.
+"""
+__author__ = u'lovexiaov'
+
+from urllib import urlencode, quote, unquote
+
+data = {
+    'name': '张三',
+    'age': 24
+}
+city = '丰台'
+
+# 键值对通过 urlencode 编码
+print(urlencode(data))  # age=24&name=%E5%BC%A0%E4%B8%89
+# 单个字符串通过 quote 编码
+print(quote(city))  # %E4%B8%B0%E5%8F%B0
+
+# 通过 unquote 解码，注意：没有 urldecode 方法
+print(unquote('age=24&name=%E5%BC%A0%E4%B8%89'))  # age=24&name=张三
+print(unquote('%E4%B8%B0%E5%8F%B0'))  # 丰台
